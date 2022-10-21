11cfec58004b1d0100e587ed415854e9aacd5486
Omkar Desai <omkar.desai@fafadiatech.com>
2022-10-21 18:14:55 +0530

    pushing github api

---
+++ git_api/core/management/commands/ingest_git_data.py
@@ -0,0 +1,50 @@
+from github import Github
+from django.core.management.base import BaseCommand
+from core.models import GitIssues,GitPullRequests
+from pathlib import Path
+
+class Command(BaseCommand):
+    help = "Ingest bootstrap git_data into MongoDB"
+
+    def handle(self, *args, **options):
+        BASE_DIR = Path(__file__).parent.resolve()
+        issues = GitIssues()
+        pull_requests = GitPullRequests()
+        
+        access_token ="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"
+
+        login  = Github(access_token)
+        user  = login.get_user()
+
+        my_repos = user.get_repos()
+        
+        for repository  in my_repos:
+            repo_name =  repository.name
+            private = repository.private
+
+            if private:
+                print(f"get pull request for repo {repo_name}")
+                pulls = repository.get_pulls(state="all") 
+                for pr in pulls:
+                    issue_number, pr_title,state,name= pr.number, pr.title,pr.state,pr.user.login
+                    pull_requests.add(
+                        {
+                            'user':name,
+                            "issue_number": issue_number,
+                            "pr_title": pr_title,
+                            "state": state,
+                            "repo_name": repo_name,
+                        }
+                    )
+                print(f"Ingested pull request")
+
+                all_issues = repository.get_issues(state='all')
+                for issue in all_issues:
+                    issues.add({
+                                'title': issue.title,
+                                'number': issue.number,
+                                'name': issue.user.login,
+                                'state': issue.state,
+                                "repo_name": repo_name,
+                                })
+                print(f"Ingested GitIssues")
---
+++ git_api/core/models.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+
+
+class MongoDBModel:
+    def encode_object_id_to_string(self, info):
+        info["_id"] = str(info["_id"])
+        return info
+
+    def add(self, info):
+        self.collection.insert_one(info)
+
+    def find(self, filter_criteria):
+        return list(self.collection.find(filter_criteria))
+
+    def list_all(self):
+        return map(self.encode_object_id_to_string, list(self.collection.find()))
+
+    def count(self):
+        return self.collection.count_documents({})
+
+    def drop(self):
+        self.collection.drop()
+
+
+class GitIssues(MongoDBModel):
+    def __init__(self):
+        self.collection = settings.DB["issues"]
+
+
+class GitPullRequests(MongoDBModel):
+    def __init__(self):
+        self.collection = settings.DB["pr"]
